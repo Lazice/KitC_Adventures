cc16168cf3f1698da10ca8e185c1013139c4b65d
ariel <gol0500@mckinnoncs.vic.edu.au>
2023-12-12 13:54:16 +1100

    hello xstate

---
+++ luci-death-game/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './Components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="game-wrapper">
+        <p>dialogueeee</p>
+      </div>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
---
+++ luci-death-game/src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Button.css'
+
+function Button() {
+  return (
+    <div>
+      <button>Option 1</button>
+    </div>
+  )
+}
+
+export default Button
